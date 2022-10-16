77f0bc1470ff35312abfaacfaf317f64912b960c
preethyvijayan <115807568+preethyvijayan@users.noreply.github.com>
2022-10-16 20:28:40 +0530

    uploading my files to repository

---
+++ ArrayDemo.java
@@ -0,0 +1,19 @@
+class ArrayDemo
+{
+public static void main(String args[])
+{
+	int[] myArray={50,60,70,80,90,100};
+	//System.out.println(myArray[0]);
+	for(int i=0;i<myArray.length;i++)//myArray.length=6 elements
+	{
+		System.out.println(myArray[i]);
+	}
+}
+}
+
+
+/*myArray
+50  60  70  80  90  100
+[0] [1] [2] [3] [4] [5]
+
+An arrays size/length is calculated by the number of elements in the array.*/
